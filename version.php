34e5d10cb31e12dd0bd57875a4a099564a1934f0
Ikrame Saadi <hello@ikramagix.com>
2025-03-14 22:09:05 +0200

    Update version to resumit due to deleted acc issue

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_yesyoucanquiz'; // MUST start with 'local_' for Moodle to detect it
 $plugin->type = 'local'; // Explicitly define as a local plugin
-$plugin->version = 2025031201;
+$plugin->version = 2025031202;
 $plugin->requires = 2023111300;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.1';
